b4820bcad54247976d535eb255724c0da20465c1
Denis Dmitriev <denitos97@gmail.com>
2021-02-18 15:20:02 +0300

    Created PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int OwnerId, String domain, String query, Boolean ownersOnly, int count, int offset) {
+        // TODO: add logic
+        return null;
+    }
+    public void delete(int ownerId, int postId) {
+        // TODO: add logic
+    }
+}
